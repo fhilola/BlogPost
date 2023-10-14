bd3dcd80d993821ac308bd32339aad9ee51d2570
fhilola <hilolafayoziddinova14@gmail.com>
2023-10-14 09:14:28 +0500

    sign up page finished

--- script/sign-up.js
+++ script/sign-up.js
@@ -4,19 +4,23 @@ const lastName = document.getElementById("lastname")
 const email = document.getElementById("email")
 const password = document.getElementById("password")
 const visibilityBtn = document.getElementById("visibility-btn")
-
+const checkFirstname = document.getElementById("check-firstname")
+const checkLastname = document.getElementById("check-lastname")
+const checkEmail = document.getElementById("check-email")
+const checkPassword = document.getElementById("check-password")
+const EMAIL_REGEX = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&_])[A-Za-z\d@$!%*#?&_]{8,}$/
 visibilityBtn.addEventListener("click", (e)=> {
     e.preventDefault()
         if(e.target.closest("#visibility-btn").previousElementSibling.type === "password"){
             e.target.closest("#visibility-btn").previousElementSibling.type = "text"
-            e.target.closest("#visibility-btn").firstElementChild.classList = "fa-solid fa-eye-slash"
+            e.target.closest("#visibility-btn").classList = "fas fa-eye-slash"
         }
         else{
            e.target.closest("#visibility-btn").previousElementSibling.type = "password"
-            e.target.closest("#visibility-btn").firstElementChild.classList = "fa-solid fa-eye" 
+           e.target.closest("#visibility-btn").classList = "fa-solid fa-eye" 
         }
 })
-
 // var myHeaders = new Headers()
 // myHeaders.append("Content-Type", "application/json")
 // var raw = JSON.stringify({
@@ -31,7 +35,60 @@ visibilityBtn.addEventListener("click", (e)=> {
 //     body: raw,
 //     redirect: 'follow'
 // }
-axios.post("http://localhost:3000/api/auth/signup", {
-    ""
+firstName.addEventListener("blur", ()=>{
+    if(firstName.value.trim().length > 10){
+        checkFirstname.textContent = "Firstname should be less than 10 letters"
+        checkFirstname.style = "display: block; color: red"
+    }else{
+        checkFirstname.style = "display: none;"
+    }
 })
-// catch(error => console.log('error', error))
+lastName.addEventListener("blur", ()=>{
+    if(lastName.value.trim().length > 10){
+        checkLastname.textContent = "Lastnsme should be less than 10 letters"
+        checkLastname.style = "display: block; color: red"
+    }else{
+        checkLastname.style = "display: none;"
+    }
+})
+email.addEventListener("blur", (e)=>{
+    const emailchecking = EMAIL_REGEX.test(e.target.value)
+    if(emailchecking == false){
+        checkEmail.textContent = "Email is not valid"
+        checkEmail.style = "display: block; color: red"
+    }else{
+        checkEmail.style = "display: none;"
+    }
+})
+password.addEventListener("input", (e)=>{
+    const passwordChecking = PASSWORD_REGEX.test(e.target.value)
+    if(passwordChecking == false){
+        checkPassword.textContent = "Minimum eight characters, at least one letter, one number and one special character"
+        checkPassword.style = "display: block; color: red; width: 300px"
+    }else{
+        checkPassword.style = "display: none;"
+    }
+})
+signupForm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    axios.post("http://localhost:3000/api/auth/signup", {
+    "firstname" : `${firstName.value}`,
+    "lastname" : `${lastName.value}`,
+    "email" : `${email.value}`,
+    "password" : `${password.value}`,
+    headers: {
+        "Content-Type" : "application/json"
+    }
+})
+.then(response => {
+    if(response.status === 201){
+        console.log("hello");
+        localStorage.setItem("access_token", response.data.token)
+    }
+})
+if(localStorage.getItem("access_token")){
+    location.replace(location.origin + "/pages/login.html")
+}else{
+    console.log("token yoq");
+}
+})
